dfd23e63e3808c1001a04ce715bd1d9d9f3708bb
Santeri Rusila <santeri.rusila@sarmadigital.com>
2020-04-16 21:58:12 +0300

    add test to the other implementation

--- test/codeTask.test.js
+++ test/codeTask.test.js
@@ -1,7 +1,7 @@
-const { getKeys } = require("../codeTask");
+const { getKeys, getKeys2 } = require("../codeTask");
 const assert = require("assert");
 
-const tests = [
+const getKeysTests = [
   {
     input: [1, 2, { foo: "bar" }],
     return: ["foo"],
@@ -52,10 +52,69 @@ const tests = [
   },
 ];
 
+const getKeys2Tests = [
+  {
+    input: [1, 2, { foo: "bar" }],
+    return: ["foo"],
+  },
+  {
+    input: [{ foo: "bar" }, { baz: { quux: "buux" } }],
+    return: ["foo", "quux", "baz"],
+  },
+  {
+    input: { title: { fi: "moi", sv: "hej", en: "hello" } },
+    return: ["fi", "sv", "en", "title"],
+  },
+  {
+    input: "moikka",
+    return: [],
+  },
+  {
+    input: {
+      1: {
+        2: "bar",
+        3: "bar",
+        4: {
+          5: {
+            6: {
+              7: "bar",
+              8: "bar",
+              9: [
+                {
+                  10: "bar",
+                  11: "bar",
+                },
+                {
+                  12: {
+                    13: "bar",
+                    14: "bar",
+                  },
+                },
+              ],
+            },
+          },
+        },
+        15: "bar",
+        16: "bar",
+        17: ["asd", "ASD", "ASD"],
+      },
+    },
+    return: [2, 3, 7, 8, 10, 11, 13, 14, 15, 16, 17, 12, 9, 6, 5, 4, 1],
+  },
+];
+
 describe("getKeys()", () => {
   it("should return object keys in correct order when JSON is given", () => {
-    tests.forEach(({ input, return: returnValue }) => {
+    getKeysTests.forEach(({ input, return: returnValue }) => {
       assert.deepEqual(getKeys(input), returnValue);
+    });
+  });
+});
+
+describe("getKeys2()", () => {
+  it("should return object keys in correct order when JSON is given", () => {
+    getKeys2Tests.forEach(({ input, return: returnValue }) => {
+      assert.deepEqual(getKeys2(input), returnValue);
     });
   });
 });
